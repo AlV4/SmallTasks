4f2f30157e17a216d5ce4d2d6bfeb7930d1db43a
AlV4 <alex187b@bk.ru>
2015-12-11 14:59:19 +0200

    Iterator created

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,32 +2,32 @@ public class Launcher {
 
     public static void main(String[] args) {
 
-//        SimpleLinkedList list = new SimpleLinkedList();
-//
-//        list.addFirst("10");
-//        list.addFirst("9");
-//        list.addFirst("8");
-//        list.addFirst("7");
-//        list.addFirst("6");
-//        list.addFirst("5");
-//        list.addFirst("4");
-//        list.addFirst("3");
-//        list.addFirst("2");
-//        list.addFirst("1");
-//        list.addLast("Last one");
-//        System.out.println(list.getSize());
-//
-//        System.out.println(list.toString());
+        SimpleLinkedList list = new SimpleLinkedList();
+
+        list.addLast("10");
+        list.addAfter("10", "Target object");
+
+        System.out.println(list.toString());
+        for (Object o: list){
+            System.out.print(o);
+        }
+        System.out.println();
+
         SimpleLinkedList list1 = new SimpleLinkedList();
 
-        list1.addFirst("2");
+        list1.addFirst("22");
         list1.addFirst("add first");
-        list1.addFirst("another one");
-        list1.addFirst("another one");
+        list1.addFirst(" one");
+        list1.addFirst("two");
         list1.addFirst("1");
-        list1.addAfter("2", "Target object");
+        list1.addLast("22");
+        list1.addAfter("22", null);
         System.out.println(list1.toString());
-        System.out.println(list1.getSize());
+        System.out.println("Size of list: "+list1.getSize());
+
+        for(Object s:list1){
+            System.out.print(s);
+        }
 
     }
 
--- src/SimpleLinkedList.java
+++ src/SimpleLinkedList.java
@@ -1,50 +1,94 @@
-public class SimpleLinkedList {
+import java.util.Iterator;
+
+public class SimpleLinkedList implements Iterable<Object> {
 
     private Node firstNode;
     private int size;
 
     @Override
     public String toString() {
-        if(firstNode != null) {
-            return firstNode.toString();
+        if (firstNode != null) {
+            Node n = firstNode;
+            String list = "";
+            while (n != null) {
+                list += n.toString();
+                n = n.nextNode;
+            }
+            return list;
         }
         return "This list is empty!";
     }
 
+    @Override
+    public Iterator iterator() {
+        return new SLLIterator();
+    }
+
     class Node {
-         public Object o;
-         Node nextNode;
+        public Object o;
+        Node nextNode;
 
         @Override
         public String toString() {
-            return "[ " + o + " ]" + "--> " + nextNode;
+            return "[ " + o + " ]" + "--> ";
         }
+    }
+
+    class SLLIterator implements Iterator {
+
+        Node n = firstNode;
+        Node firstElement;
+
+        public Node next() {
+            if (hasNext() && firstElement != null) {
+                n = n.nextNode;
+                return n;
+            }
+            firstElement = firstNode;
+            if(firstElement.nextNode == null){
+                n = null;
+            }
+            return firstElement;
+        }
+
+        @Override
+        public void remove() {
+
+        }
+
+        public boolean hasNext() {
+            if (n == null || (n.nextNode == null && n!=firstNode)) {
+                return false;
+            }
+            return true;
+        }
+
     }
 
     public SimpleLinkedList() {
         size = 0;
     }
 
-    public void addFirst(Object o){
+    public void addFirst(Object o) {
         Node node = new Node();
         node.o = o;
-        if(firstNode != null){
+        if (firstNode != null) {
             node.nextNode = firstNode;
         }
         firstNode = node;
         size++;
     }
 
-    public void addLast(Object o){
+    public void addLast(Object o) {
         Node lastNode;
         Node node = new Node();
         node.o = o;
-        if(firstNode == null){
-           firstNode = node;
+        if (firstNode == null) {
+            firstNode = node;
 
-        }else {
+        } else {
             lastNode = firstNode;
-            while (lastNode.nextNode != null){
+            while (lastNode.nextNode != null) {
                 lastNode = lastNode.nextNode;
             }
             lastNode.nextNode = node;
@@ -52,35 +96,33 @@ public class SimpleLinkedList {
         size++;
     }
 
-    public void addAfter(Object afterWho, Object target){
-        if (firstNode == null){
+    public void addAfter(Object afterWho, Object target) {
+        if (firstNode == null) {
             System.out.println("This list is empty!");
             return;
         }
         Node n = new Node();
         n.o = target;
         Node currentNode = firstNode;
-        if(currentNode.o.equals(afterWho)){
+        if (currentNode.o.equals(afterWho)) {
             n.nextNode = currentNode.nextNode;
             currentNode.nextNode = n;
             size++;
             return;
         }
-        while (!currentNode.o.equals(afterWho)){
+        while (!currentNode.o.equals(afterWho)) {
             currentNode = currentNode.nextNode;
-            if(!currentNode.o.equals(afterWho) && currentNode.nextNode == null){
+            if (!currentNode.o.equals(afterWho) && currentNode.nextNode == null) {
                 throw new IllegalStateException("There is no such elements!");
             }
         }
-
-            n.nextNode = currentNode.nextNode;
-            currentNode.nextNode = n;
-
+        n.nextNode = currentNode.nextNode;
+        currentNode.nextNode = n;
         size++;
-        System.out.println("Element successfully added!");
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
+
 }
