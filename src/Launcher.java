699bd9c678395a5aa82e355b341e8e8878d206e4
AlV4 <alex187b@bk.ru>
2015-12-25 15:10:26 +0200

    comparator created

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,10 @@
-import shapes.*;
-import shapes.Rectangle;
 
 
 public class Launcher {
 
     public static void main(String[] args) {
 
-        Drawable[]shapes = {new Circle(), new Rectangle(), new Triangle(), new Square()};
-        new ShapesTemplate(shapes);
+
     }
 
 }
