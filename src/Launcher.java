000f15a0eb5b8da73e4588e5b88ae9104a39cd66
AlV4 <alex187b@bk.ru>
2016-02-04 14:52:08 +0200

    Container for different types created

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,48 +1,23 @@
-import java.util.ArrayList;
-
 public class Launcher {
 
     public static void main(String[] args) {
 
-        Subscriber subscriber1 = new Subscriber("First", " some eMail", "someAddress");
-        Subscriber subscriber2 = new Subscriber("Second", " some eMail", "someAddress");
-        Subscriber subscriber3 = new Subscriber("Third", " some eMail", "someAddress");
-        Subscriber subscriber4 = new Subscriber("Fourth", " some eMail", "someAddress");
-        Subscriber subscriber5 = new Subscriber("Main", " some eMail", "someAddress");
+       Integer integer = 10;
+        MyOwnBox<Integer> box = new MyOwnBox<>();
 
-        PublishingOffice office = new PublishingOffice();
+        box.add(integer);
+        box.add(2);
+        box.add(6);
+        box.print();
+        box.remove(integer);
+        box.print();
 
-        office.subscribe(subscriber1, office.editions.get(PublishingOffice.MENS));
-
-        ArrayList <AbstractPrintingEdition> editionArr =  new ArrayList<>();
-        editionArr.add(office.editions.get(PublishingOffice.MENS));
-        editionArr.add(office.editions.get(PublishingOffice.GEO));
-        office.subscribe(subscriber2, editionArr);
-
-        ArrayList<AbstractPrintingEdition> editionArr1 =  new ArrayList<>();
-        editionArr1.add(office.editions.get(PublishingOffice.COSMO));
-        editionArr1.add(office.editions.get(PublishingOffice.TIMES));
-        editionArr1.add(office.editions.get(PublishingOffice.DAILY));
-        office.subscribe(subscriber3, editionArr1);
-
-        office.subscribe(subscriber4, office.editions.get(PublishingOffice.DAILY));
-
-        office.subscribe(subscriber5, office.editions);
-
-        for (AbstractPrintingEdition edition : office.editions){
-            office.notifySubscribers(edition);
-            System.out.println("----------------");
-        }
-
-        office.unSubscribe(subscriber3, office.editions.get(PublishingOffice.TIMES));
-        office.unSubscribeFromAll(subscriber5);
-        System.out.println("================================");
-
-        for (AbstractPrintingEdition edition : office.editions){
-            office.notifySubscribers(edition);
-            System.out.println("----------------");
-        }
-
-
+        MyOwnBox<String> strBox = new MyOwnBox<>();
+        strBox.add("One");
+        strBox.add("Two");
+        strBox.add("Good IDEA!");
+        strBox.print();
+        strBox.remove("Two");
+        strBox.print();
     }
 }
---
+++ src/MyOwnBox.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MyOwnBox<T>{
+    private ArrayList <T> listOfThings;
+
+    public MyOwnBox() {
+        listOfThings = new ArrayList<>();
+    }
+
+    public void print(){
+        System.out.println(Arrays.asList(listOfThings));
+    }
+
+    public void add(T t){
+        listOfThings.add(t);
+    }
+
+    public T get(int idx){
+        return listOfThings.get(idx);
+    }
+
+    public void remove(T t){
+        listOfThings.remove(t);
+    }
+
+    public ArrayList<T> getListOfThings() {
+        return listOfThings;
+    }
+}
