6f988dbf0579e6f4992bda6dd745805467f86348
AlV4 <alex187b@bk.ru>
2016-02-11 10:16:33 +0200

    class info method created

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,23 +1,28 @@
 import service.Duck;
-import service.Eagle;
-import service.Service;
-import service.ServiceRepository;
 
 public class Launcher {
 
     public static void main(String[] args) {
 
-        ServiceRepository<Service> box = new ServiceRepository();
+        printClassInfo(new Duck("AAA", 5).getClass());
+        System.out.println();
+        Class clazz = new ArrayIndexOutOfBoundsException().getClass();
+        printClassInfo(clazz);
+        clazz = new IllegalArgumentException().getClass();
+        System.out.println();
+        printClassInfo(clazz);
 
-        Duck duck = new Duck("Aaaa", 1);
-        Eagle eagle = new Eagle("Bbbb",2);
+    }
 
-        box.add(duck);
-        box.add(eagle);
+    public static void printClassInfo(Class c){
 
-        for (Service s : box.getStorage()){
-            System.out.println(s);
+        System.out.println("Class: =========== " + c.getSimpleName() + "===========");
+        System.out.println("Class full name: " + c.getName());
+        if(c.getSuperclass() != null){
+            c = c.getSuperclass();
+            System.out.println("Parent class name: " + c.getName());
+            System.out.println("Up ierarchy class info: ");
+            printClassInfo(c);
         }
-
     }
 }
