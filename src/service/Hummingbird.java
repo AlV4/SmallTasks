edef11690bcdc217311196062f5fb5b112aab48e
AlV4 <alex187b@bk.ru>
2016-02-23 14:58:48 +0200

    ApplicationManager tests created

--- src/service/Hummingbird.java
+++ src/service/Hummingbird.java
@@ -43,4 +43,32 @@ public class Hummingbird extends AbstractBird {
     public void setAdditionalWeight(double additionalWeight) {
         this.additionalWeight = additionalWeight;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Hummingbird)) return false;
+        if (!super.equals(o)) return false;
+
+        Hummingbird that = (Hummingbird) o;
+
+        if (getSecretAmount() != that.getSecretAmount()) return false;
+        if (Double.compare(that.getAnotherPrice(), getAnotherPrice()) != 0) return false;
+        if (Double.compare(that.getAdditionalWeight(), getAdditionalWeight()) != 0) return false;
+        return !(getAnotherName() != null ? !getAnotherName().equals(that.getAnotherName()) : that.getAnotherName() != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        long temp;
+        result = 31 * result + (getAnotherName() != null ? getAnotherName().hashCode() : 0);
+        result = 31 * result + getSecretAmount();
+        temp = Double.doubleToLongBits(getAnotherPrice());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(getAdditionalWeight());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
--- tests/service/InstanceCreatorTest.java
+++ tests/service/InstanceCreatorTest.java
@@ -1,24 +1,31 @@
 package service;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(JUnit4.class)
 public class InstanceCreatorTest {
 
     ArrayList<Object>list;
     InstanceCreator creator;
+    Map<String, Object> map;
 
     @Before
     public void initTest(){
 
         creator = new InstanceCreator();
         list = new ArrayList<>();
+        map = new HashMap<>();
     }
     @Test
     public void correctParameterInitClassWithListTest(){
@@ -29,7 +36,7 @@ public class InstanceCreatorTest {
            c = (Integer) creator.initClass(i.getClass(), list);
         } catch (Exception e) {
         }
-        Assert.assertEquals(c,i);
+        assertEquals(c,i);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -54,7 +61,7 @@ public class InstanceCreatorTest {
         list.add(3, 33.3d);
         chicken1 = (Chicken) creator.initClass(chicken1.getClass(), list);
         chicken2 = (Chicken) creator.initClass(chicken2.getClass(), list);
-        Assert.assertEquals(chicken1, chicken2);
+        assertEquals(chicken1, chicken2);
     }
 
     @Test
@@ -68,7 +75,54 @@ public class InstanceCreatorTest {
         chicken1 = (Chicken) creator.initClass(chicken1.getClass(), list);
         list.removeAll(list);
         chicken2 = (Chicken) creator.initClass(chicken2.getClass(), list);
-        Assert.assertNotEquals(chicken1, chicken2);
+        assertNotEquals(chicken1, chicken2);
     }
 
+    @Test
+    public void setPrivatesNotEqualsTest() throws Exception{
+        Hummingbird h1 = new Hummingbird();
+        creator.setPrivates(h1, map);
+        map.put("anotherName", "Some name");
+        Hummingbird h2 = new Hummingbird();
+        creator.setPrivates(h2, map);
+        assertNotEquals(h1, h2);
+    }
+
+    @Test
+    public void setPrivatesEqualsTest() throws Exception{
+        map.put("anotherName", "Some name");
+        Hummingbird h1 = new Hummingbird();
+        creator.setPrivates(h1, map);
+        Hummingbird h2 = new Hummingbird();
+        creator.setPrivates(h2, map);
+        assertEquals(h1, h2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setPrivatesIAExNameTest() throws Exception{
+        map.put("anotherName", 1);
+        Hummingbird h1 = new Hummingbird();
+        creator.setPrivates(h1, map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setPrivatesIAExAmountTest() throws Exception{
+        map.put("secretAmount", "Str");
+        Hummingbird h1 = new Hummingbird();
+        creator.setPrivates(h1, map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setPrivatesIAExPriceTest() throws Exception{
+        map.put("anotherPrice", new JFrame());
+        Hummingbird h1 = new Hummingbird();
+        creator.setPrivates(h1, map);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setPrivatesIAExWeightTest() throws Exception{
+        map.put("additionalWeight", "Str");
+        Hummingbird h1 = new Hummingbird();
+        creator.setPrivates(h1, map);
+    }
 }
