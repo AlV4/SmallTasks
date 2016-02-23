1427dca7dd23a984f4bdd6220aada288fe1f929a
AlV4 <alex187b@bk.ru>
2016-02-23 18:17:28 +0200

    print stream data created

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,7 +4,7 @@ public class Launcher {
 
     public static void main(String[] args) throws Exception {
 
-        byte[] bytes = {102, 16, 35, 0, -1, 12, 18, -40, -58, 113, 2};
+        byte[] bytes = {102, 16, 35, 0, -1, 12, 18, -40, -58, 113, 2, 0};
 
         ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
 
