89f175f6faaae0ca31ea4786afa594a21889de1b
AlV4 <alex187b@bk.ru>
2016-02-23 18:15:01 +0200

    ApplicationManager tests created

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,35 +1,23 @@
-import service.AbstractBird;
-import service.ApplicationManager;
-import service.Chicken;
-import service.Hummingbird;
-
-import java.lang.reflect.Field;
+import java.io.ByteArrayInputStream;
 
 public class Launcher {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        ApplicationManager<AbstractBird> am = new ApplicationManager();
-        System.out.println(am.getClass().toString());
-        Hummingbird hum = new Hummingbird();
-        am.printAnnotatedMethod(hum.getClass());
-        AbstractBird chicken = new Chicken();
-        am.printAnnotatedMethod(chicken.getClass());
+        byte[] bytes = {102, 16, 35, 0, -1, 12, 18, -40, -58, 113, 2};
 
-        Chicken chicken3 = (Chicken) am.getService(chicken.getClass());
-        System.out.println(chicken3);
+        ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
 
+        printStreamData(stream);
     }
 
-    public static void fieldPrinter(Object o) throws NoSuchFieldException, IllegalAccessException {
-        Class<?> clazz = o.getClass();
-        System.out.println("========" + clazz.getSimpleName() + " private fields ===========");
-        Field[]fields = clazz.getDeclaredFields();
-        for (Field f:fields){
-            if(!f.isAccessible()){
-                f.setAccessible(true);
-                System.out.println("Field: " + f.getName() +  ", type: " + f.getType() + ", value: " + f.get(o));
-            }
+    public static void printStreamData(ByteArrayInputStream stream) {
+        int i;
+        System.out.print("[ ");
+        while ((i = stream.read()) != -1) {
+            byte b = (byte) i;
+            System.out.print(b + " ");
         }
+        System.out.println("]");
     }
 }
--- src/service/Duck.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-public class Duck extends AbstractBird {
-    public Duck(String name, int amount) {
-        super(name, amount);
-    }
-}
