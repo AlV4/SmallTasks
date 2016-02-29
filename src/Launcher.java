cc490249e403c485e4ee0a8df7001ea53e75e08a
AlV4 <alex187b@bk.ru>
2016-02-29 18:47:24 +0200

    printStreamData changed, now it accepts an InputStream and writes strings from file to console

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,10 +5,6 @@ public class Launcher {
 
     public static void main(String[] args) throws Exception {
 
-        byte[] bytes = {102, 16, 35, 0, -1, 12, 18, -40, -58, 113, 2, 0};
-
-        ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
-
         File file = new File("file.txt");
         java.nio.file.Path path = file.toPath();
 
