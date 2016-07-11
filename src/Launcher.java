8164b22fe1c7cf6ba7068c21d9dfe792c665c102
AlV4 <alex187b@bk.ru>
2016-07-11 22:14:58 +0300

    system out redirected to a file

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,14 +1,17 @@
 import java.io.FileOutputStream;
-import java.io.PrintStream;
 
 public class Launcher {
 
     public static void main(String[] args) throws Exception {
-        PrintStream out = new PrintStream(new FileOutputStream("console.txt",true));
-        System.setOut(out);
+        FileOutputStream file = new FileOutputStream("console.txt", true);
+        TeePrintStream tee = new TeePrintStream(file, System.out);
+        System.setOut(tee);
+
+//        PrintStream out = new PrintStream(new FileOutputStream("console.txt",true));
+//        System.setOut(out);
         for(int i = 1; i <= 10; i++) {
             System.out.println("Sometext " + i + " times");
         }
-        out.close();
+//        out.close();
    }
 }
